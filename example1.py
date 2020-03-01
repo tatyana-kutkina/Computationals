0fe7a699540e9d8dac80a1ee9bdf6ed35f72d162
tatyana-kutkina <kutkina_tu@gkl-kemerovo.ru>
2020-03-01 12:34:17 +0300

    a half of the first task

--- example1.py
+++ example1.py
@@ -10,7 +10,7 @@ print('Порядок интерполирования n = 3', '\n')
 
 # определим фукнцию f
 def f(x):
-    return round(math.cos(x), 4)
+    return round(x**3+2)
 
 
 # разделенные разности
@@ -74,7 +74,7 @@ def estimate_mod_der(n, x, nodes):
         for j in range(i + 1):
             c = min(c, nodes[j])
             d = max(d, nodes[j])
-        step = abs(c - d) / 50
+        step = abs(c - d) / 2000
         for k in np.arange(c + step, d, step):
             modules[i] = max(abs(derivative(f, k, dx=1.0, n=i + 1, order=n + 2)),
                              abs(derivative(f, k - step, dx=1.0, n=i + 1, order=n + 2)))
@@ -90,13 +90,12 @@ def estimate_err(x, modules, nodes):
     return est_errors
 
 
-#main
 # вводим значения и сортируем массив узлов
 n = 3
-x = -0.4
+x = 2
 # массив узлов
 
-arr1 = -0.6, -0.5, -0.3, -0.2, -0.1, 0
+arr1 = -2, 0, 1, 3, 4, 5
 
 arr2 = []  # массив расстояний между точкой интерполирования и узлами
 for i in arr1:
@@ -120,8 +119,31 @@ xx = []
 for i in X:
     xx.append(i[0])
 
+'''
+for i in xx:
+    print(i)
+'''
 
-# печать таблицы разделенных разностей
+'''
+# разделенные разности
+r1_1_2 = (f(xx[2]) - (f(xx[1]))) / (xx[2] - xx[1])
+r1_2_3 = (f(xx[3]) - f(xx[2])) / (xx[3] - xx[2])
+r2_1_2_3 = (r1_2_3 - r1_1_2) / (xx[3] - xx[1])
+
+f0 = f(xx[0])
+f1 = (f(xx[1]) - f0) / (xx[1] - xx[0])
+f2 = (r1_1_2 - f1) / (xx[2] - xx[0])
+f3 = (r2_1_2_3 - f2) / (xx[3] - xx[0])
+
+p0 = f0
+p1 = p0 + f1 * (x - xx[0])
+p2 = p1 + f2 * (x - xx[0]) * (x - xx[1])
+p3 = p2 + f3 * (x - xx[0]) * (x - xx[1]) * (x - xx[2])
+'''
+
+# print(p0, p1, p2, p3)
+
+# main
 r1, r2, r3 = fun(xx)
 printR(r1, r2, r3, xx)
 
@@ -129,7 +151,7 @@ printR(r1, r2, r3, xx)
 print('\n')
 print('{0:<60}'.format('i'), end=' ')
 for i in range(n+1):
-    print('{0:^8}'.format(i),end=' ')
+    print('{0:<8}'.format(i),end=' ')
 print('\n')
 
 print('{0:<60}'.format('Узлы в порядке очередности их использования'), end=' ')
